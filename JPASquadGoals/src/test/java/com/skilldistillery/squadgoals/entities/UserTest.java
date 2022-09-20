5d1b1e1e4843bfcf77de17ed6cf7859a6a8f918e
GoldilocksZone <jmarulli@gmail.com>
2022-09-20 19:59:41 -0400

    Green JUnit test for User

--- JPASquadGoals/src/test/java/com/skilldistillery/squadgoals/entities/UserTest.java
+++ JPASquadGoals/src/test/java/com/skilldistillery/squadgoals/entities/UserTest.java
@@ -1,6 +1,11 @@
 package com.skilldistillery.squadgoals.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -9,26 +14,41 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UserTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private User user;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPASquadGoals");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		user = em.find(User.class, Long.valueOf(1));
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		user = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_user_not_null() {
+		assertNotNull(user);
+	}
+	
+	@Test
+	void test_user_mappings() {
+		assertEquals(user.getUsername(), "originaltom");
+		assertEquals(user.getPassword(), "myspace");
 	}
 
 }
