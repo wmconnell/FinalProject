e8ae4c3a1a5283c9209d54c4d5738f1c83820f41
GoldilocksZone <jmarulli@gmail.com>
2022-09-21 18:14:14 -0400

    DB modifications

--- DB/squadgoalsdb.sql
+++ DB/squadgoalsdb.sql
@@ -16,48 +16,6 @@ CREATE SCHEMA IF NOT EXISTS `squadgoalsdb` DEFAULT CHARACTER SET utf8 ;
 USE `squadgoalsdb` ;
 
 -- -----------------------------------------------------
--- Table `goal`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `goal` ;
-
-CREATE TABLE IF NOT EXISTS `goal` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(99) NULL,
-  `description` TEXT NULL,
-  `created_date` DATETIME NULL,
-  `updated_date` DATETIME NULL,
-  `completed_date` DATETIME NULL,
-  `start_date` DATETIME NULL,
-  `end_date` DATETIME NULL,
-  `completed` TINYINT NULL,
-  `visibility` VARCHAR(45) NULL,
-  `attendance` VARCHAR(45) NULL,
-  `recurring` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `review`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `review` ;
-
-CREATE TABLE IF NOT EXISTS `review` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `rating` INT NULL,
-  `comment` TEXT NULL,
-  `goal_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_review_goal1_idx` (`goal_id` ASC),
-  CONSTRAINT `fk_review_goal1`
-    FOREIGN KEY (`goal_id`)
-    REFERENCES `goal` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `image`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `image` ;
@@ -65,21 +23,7 @@ DROP TABLE IF EXISTS `image` ;
 CREATE TABLE IF NOT EXISTS `image` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `url` VARCHAR(499) NULL,
-  `goal_id` INT NULL DEFAULT NULL,
-  `review_id` INT NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_image_goal1_idx` (`goal_id` ASC),
-  INDEX `fk_image_review1_idx` (`review_id` ASC),
-  CONSTRAINT `fk_image_goal1`
-    FOREIGN KEY (`goal_id`)
-    REFERENCES `goal` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_image_review1`
-    FOREIGN KEY (`review_id`)
-    REFERENCES `review` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
@@ -98,8 +42,8 @@ CREATE TABLE IF NOT EXISTS `user` (
   `role` VARCHAR(45) NULL,
   `bio` TEXT NULL,
   `active` TINYINT NULL,
-  `points` INT NULL,
   `profile_image_id` INT NOT NULL,
+  `create_date` DATETIME NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC),
@@ -123,8 +67,8 @@ CREATE TABLE IF NOT EXISTS `squad` (
   `bio` TEXT NULL,
   `active` TINYINT NULL,
   `leader_id` INT NOT NULL,
-  `points` INT NULL,
   `profile_image_id` INT NOT NULL,
+  `create_date` DATETIME NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `name_UNIQUE` (`name` ASC),
   INDEX `fk_squad_user1_idx` (`leader_id` ASC),
@@ -167,6 +111,63 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
+-- Table `goal`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `goal` ;
+
+CREATE TABLE IF NOT EXISTS `goal` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(99) NULL,
+  `description` TEXT NULL,
+  `created_date` DATETIME NULL,
+  `updated_date` DATETIME NULL,
+  `completed_date` DATETIME NULL,
+  `start_date` DATETIME NULL,
+  `end_date` DATETIME NULL,
+  `completed` TINYINT NULL,
+  `public_visibility` TINYINT NULL,
+  `public_attendance` TINYINT NULL,
+  `recurring` VARCHAR(45) NULL,
+  `creator_id` INT NOT NULL,
+  `active` TINYINT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_goal_user1_idx` (`creator_id` ASC),
+  CONSTRAINT `fk_goal_user1`
+    FOREIGN KEY (`creator_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `review`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `review` ;
+
+CREATE TABLE IF NOT EXISTS `review` (
+  `rating` INT NULL,
+  `comment` TEXT NULL,
+  `goal_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `review_date` DATETIME NULL,
+  INDEX `fk_review_goal1_idx` (`goal_id` ASC),
+  INDEX `fk_review_user1_idx` (`user_id` ASC),
+  PRIMARY KEY (`goal_id`, `user_id`),
+  CONSTRAINT `fk_review_goal1`
+    FOREIGN KEY (`goal_id`)
+    REFERENCES `goal` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_review_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `squad_has_goal`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `squad_has_goal` ;
@@ -174,6 +175,7 @@ DROP TABLE IF EXISTS `squad_has_goal` ;
 CREATE TABLE IF NOT EXISTS `squad_has_goal` (
   `squad_id` INT NOT NULL,
   `goal_id` INT NOT NULL,
+  `points` INT NULL,
   PRIMARY KEY (`squad_id`, `goal_id`),
   INDEX `fk_squad_has_goal_goal1_idx` (`goal_id` ASC),
   INDEX `fk_squad_has_goal_squad1_idx` (`squad_id` ASC),
@@ -357,6 +359,7 @@ DROP TABLE IF EXISTS `user_has_task` ;
 CREATE TABLE IF NOT EXISTS `user_has_task` (
   `user_id` INT NOT NULL,
   `task_id` INT NOT NULL,
+  `points` INT NULL,
   PRIMARY KEY (`user_id`, `task_id`),
   INDEX `fk_user_has_task_task1_idx` (`task_id` ASC),
   INDEX `fk_user_has_task_user1_idx` (`user_id` ASC),
@@ -381,6 +384,7 @@ DROP TABLE IF EXISTS `squad_has_task` ;
 CREATE TABLE IF NOT EXISTS `squad_has_task` (
   `squad_id` INT NOT NULL,
   `task_id` INT NOT NULL,
+  `points` INT NULL,
   PRIMARY KEY (`squad_id`, `task_id`),
   INDEX `fk_squad_has_task_task1_idx` (`task_id` ASC),
   INDEX `fk_squad_has_task_squad1_idx` (`squad_id` ASC),
@@ -406,7 +410,6 @@ CREATE TABLE IF NOT EXISTS `badge` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NULL,
   `description` VARCHAR(499) NULL,
-  `conditions` VARCHAR(499) NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
@@ -419,6 +422,7 @@ DROP TABLE IF EXISTS `badge_has_squad` ;
 CREATE TABLE IF NOT EXISTS `badge_has_squad` (
   `badge_id` INT NOT NULL,
   `squad_id` INT NOT NULL,
+  `achieved_date` DATETIME NULL,
   PRIMARY KEY (`badge_id`, `squad_id`),
   INDEX `fk_badge_has_squad_squad1_idx` (`squad_id` ASC),
   INDEX `fk_badge_has_squad_badge1_idx` (`badge_id` ASC),
@@ -443,6 +447,7 @@ DROP TABLE IF EXISTS `badge_has_user` ;
 CREATE TABLE IF NOT EXISTS `badge_has_user` (
   `badge_id` INT NOT NULL,
   `user_id` INT NOT NULL,
+  `achieved_date` DATETIME NULL,
   PRIMARY KEY (`badge_id`, `user_id`),
   INDEX `fk_badge_has_user_user1_idx` (`user_id` ASC),
   INDEX `fk_badge_has_user_badge1_idx` (`badge_id` ASC),
@@ -454,6 +459,107 @@ CREATE TABLE IF NOT EXISTS `badge_has_user` (
   CONSTRAINT `fk_badge_has_user_user1`
     FOREIGN KEY (`user_id`)
     REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `requirement`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `requirement` ;
+
+CREATE TABLE IF NOT EXISTS `requirement` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `badge_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_condition_badge1_idx` (`badge_id` ASC),
+  CONSTRAINT `fk_condition_badge1`
+    FOREIGN KEY (`badge_id`)
+    REFERENCES `badge` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `image_has_goal`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `image_has_goal` ;
+
+CREATE TABLE IF NOT EXISTS `image_has_goal` (
+  `image_id` INT NOT NULL,
+  `goal_id` INT NOT NULL,
+  PRIMARY KEY (`image_id`, `goal_id`),
+  INDEX `fk_image_has_goal_goal1_idx` (`goal_id` ASC),
+  INDEX `fk_image_has_goal_image1_idx` (`image_id` ASC),
+  CONSTRAINT `fk_image_has_goal_image1`
+    FOREIGN KEY (`image_id`)
+    REFERENCES `image` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_image_has_goal_goal1`
+    FOREIGN KEY (`goal_id`)
+    REFERENCES `goal` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `image_has_review`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `image_has_review` ;
+
+CREATE TABLE IF NOT EXISTS `image_has_review` (
+  `image_id` INT NOT NULL,
+  `review_goal_id` INT NOT NULL,
+  `review_user_id` INT NOT NULL,
+  PRIMARY KEY (`image_id`, `review_goal_id`, `review_user_id`),
+  INDEX `fk_image_has_review_review1_idx` (`review_goal_id` ASC, `review_user_id` ASC),
+  INDEX `fk_image_has_review_image1_idx` (`image_id` ASC),
+  CONSTRAINT `fk_image_has_review_image1`
+    FOREIGN KEY (`image_id`)
+    REFERENCES `image` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_image_has_review_review1`
+    FOREIGN KEY (`review_goal_id` , `review_user_id`)
+    REFERENCES `review` (`goal_id` , `user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `squad_message`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `squad_message` ;
+
+CREATE TABLE IF NOT EXISTS `squad_message` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `message_date` DATETIME NULL,
+  `content` TEXT NULL,
+  `sender_id` INT NOT NULL,
+  `squad_id` INT NOT NULL,
+  `reply_to_id` INT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_squad_message_user1_idx` (`sender_id` ASC),
+  INDEX `fk_squad_message_squad1_idx` (`squad_id` ASC),
+  INDEX `fk_squad_message_squad_message1_idx` (`reply_to_id` ASC),
+  CONSTRAINT `fk_squad_message_user1`
+    FOREIGN KEY (`sender_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_squad_message_squad1`
+    FOREIGN KEY (`squad_id`)
+    REFERENCES `squad` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_squad_message_squad_message1`
+    FOREIGN KEY (`reply_to_id`)
+    REFERENCES `squad_message` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -476,24 +582,13 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
--- Data for table `goal`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `squadgoalsdb`;
-INSERT INTO `goal` (`id`, `title`, `description`, `created_date`, `updated_date`, `completed_date`, `start_date`, `end_date`, `completed`, `visibility`, `attendance`, `recurring`) VALUES (1, 'Pizza party', 'Some wholesome, after-school fun!', '2022-09-20 19:54:01', '2022-09-20 19:54:01', NULL, '2022-09-23 19:30:00', '2022-09-23 21:00:00', 0, 'public', 'public', NULL);
-INSERT INTO `goal` (`id`, `title`, `description`, `created_date`, `updated_date`, `completed_date`, `start_date`, `end_date`, `completed`, `visibility`, `attendance`, `recurring`) VALUES (2, 'Join Squad Goals', 'The first step', NULL, NULL, NULL, NULL, NULL, 1, 'public', 'public', NULL);
-
-COMMIT;
-
-
--- -----------------------------------------------------
 -- Data for table `image`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `squadgoalsdb`;
-INSERT INTO `image` (`id`, `url`, `goal_id`, `review_id`) VALUES (1, 'https://pbs.twimg.com/profile_images/1237550450/mstom_400x400.jpg', NULL, NULL);
-INSERT INTO `image` (`id`, `url`, `goal_id`, `review_id`) VALUES (2, 'https://static.wikia.nocookie.net/godzilla/images/3/33/Godzilla_2021.jpg/revision/latest?cb=20210314011302', NULL, NULL);
-INSERT INTO `image` (`id`, `url`, `goal_id`, `review_id`) VALUES (3, 'https://static.wikia.nocookie.net/snl/images/6/66/Wild_and_crazy_guys.jpg/revision/latest?cb=20140804162910', NULL, NULL);
+INSERT INTO `image` (`id`, `url`) VALUES (1, 'https://pbs.twimg.com/profile_images/1237550450/mstom_400x400.jpg');
+INSERT INTO `image` (`id`, `url`) VALUES (2, 'https://static.wikia.nocookie.net/godzilla/images/3/33/Godzilla_2021.jpg/revision/latest?cb=20210314011302');
+INSERT INTO `image` (`id`, `url`) VALUES (3, 'https://static.wikia.nocookie.net/snl/images/6/66/Wild_and_crazy_guys.jpg/revision/latest?cb=20140804162910');
 
 COMMIT;
 
@@ -503,8 +598,8 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `squadgoalsdb`;
-INSERT INTO `user` (`id`, `username`, `password`, `email`, `first_name`, `last_name`, `role`, `bio`, `active`, `points`, `profile_image_id`) VALUES (1, 'originaltom', 'myspace', 'tom@myspace.com', 'Tom', 'MySpace', 'admin', 'Hi, I\'m Tom, and I\'m friends with everyone!', 1, 300, 1);
-INSERT INTO `user` (`id`, `username`, `password`, `email`, `first_name`, `last_name`, `role`, `bio`, `active`, `points`, `profile_image_id`) VALUES (2, 'godzilla', 'godzilla', 'godzilla@monster.rawr', 'Go', 'Shira', 'member', 'Hi, name\'s Godzilla. Love smashing cities and chasing damsels up skyscrapers. Oh, is that my cousin, King Kong? Oops.', 1, 10, 2);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `first_name`, `last_name`, `role`, `bio`, `active`, `profile_image_id`, `create_date`) VALUES (1, 'originaltom', 'myspace', 'tom@myspace.com', 'Tom', 'MySpace', 'admin', 'Hi, I\'m Tom, and I\'m friends with everyone!', 1, 1, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `email`, `first_name`, `last_name`, `role`, `bio`, `active`, `profile_image_id`, `create_date`) VALUES (2, 'godzilla', 'godzilla', 'godzilla@monster.rawr', 'Go', 'Shira', 'member', 'Hi, name\'s Godzilla. Love smashing cities and chasing damsels up skyscrapers. Oh, is that my cousin, King Kong? Oops.', 1, 2, NULL);
 
 COMMIT;
 
@@ -514,7 +609,7 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `squadgoalsdb`;
-INSERT INTO `squad` (`id`, `name`, `bio`, `active`, `leader_id`, `points`, `profile_image_id`) VALUES (1, 'The OGs', 'We\'re just two wild and crazy guys!', 1, 1, 150, 3);
+INSERT INTO `squad` (`id`, `name`, `bio`, `active`, `leader_id`, `profile_image_id`, `create_date`) VALUES (1, 'The OGs', 'We\'re just two wild and crazy guys!', 1, 1, 3, NULL);
 
 COMMIT;
 
@@ -531,11 +626,22 @@ COMMIT;
 
 
 -- -----------------------------------------------------
+-- Data for table `goal`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `squadgoalsdb`;
+INSERT INTO `goal` (`id`, `title`, `description`, `created_date`, `updated_date`, `completed_date`, `start_date`, `end_date`, `completed`, `public_visibility`, `public_attendance`, `recurring`, `creator_id`, `active`) VALUES (1, 'Pizza party', 'Some wholesome, after-school fun!', '2022-09-20 19:54:01', '2022-09-20 19:54:01', NULL, '2022-09-23 19:30:00', '2022-09-23 21:00:00', 0, 1, 1, NULL, DEFAULT, NULL);
+INSERT INTO `goal` (`id`, `title`, `description`, `created_date`, `updated_date`, `completed_date`, `start_date`, `end_date`, `completed`, `public_visibility`, `public_attendance`, `recurring`, `creator_id`, `active`) VALUES (2, 'Join Squad Goals', 'The first step', NULL, NULL, NULL, NULL, NULL, 1, 1, 1, NULL, DEFAULT, NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
 -- Data for table `squad_has_goal`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `squadgoalsdb`;
-INSERT INTO `squad_has_goal` (`squad_id`, `goal_id`) VALUES (1, 1);
+INSERT INTO `squad_has_goal` (`squad_id`, `goal_id`, `points`) VALUES (1, 1, NULL);
 
 COMMIT;
 
@@ -579,8 +685,8 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `squadgoalsdb`;
-INSERT INTO `badge` (`id`, `name`, `description`, `conditions`) VALUES (1, 'Super Squad', 'Any squad that reaches 100 pts receives the Super Squad badge!', '{point_threshold: 100}');
-INSERT INTO `badge` (`id`, `name`, `description`, `conditions`) VALUES (2, 'Mother Teresa', 'Any squad that achieves 3 charitable goals within 1 year receives the Mother Teresa badge!', '{tags:[\'charitable\'], timeframe: 12}');
+INSERT INTO `badge` (`id`, `name`, `description`) VALUES (1, 'Super Squad', 'Any squad that reaches 100 pts receives the Super Squad badge!');
+INSERT INTO `badge` (`id`, `name`, `description`) VALUES (2, 'Mother Teresa', 'Any squad that achieves 3 charitable goals within 1 year receives the Mother Teresa badge!');
 
 COMMIT;
 
@@ -590,7 +696,7 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `squadgoalsdb`;
-INSERT INTO `badge_has_squad` (`badge_id`, `squad_id`) VALUES (1, 1);
+INSERT INTO `badge_has_squad` (`badge_id`, `squad_id`, `achieved_date`) VALUES (1, 1, NULL);
 
 COMMIT;
 
