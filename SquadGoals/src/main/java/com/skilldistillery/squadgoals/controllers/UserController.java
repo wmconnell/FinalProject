67def22db26b695fd017cbc936bfbd56e0d0995e
GoldilocksZone <jmarulli@gmail.com>
2022-09-23 18:38:16 -0400

    Completed user service, service implementation, controller classes, and postman

--- SquadGoals/src/main/java/com/skilldistillery/squadgoals/controllers/UserController.java
+++ SquadGoals/src/main/java/com/skilldistillery/squadgoals/controllers/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.squadgoals.entities.User;
+import com.skilldistillery.squadgoals.services.AuthService;
 import com.skilldistillery.squadgoals.services.UserService;
 
 @RestController
@@ -25,22 +26,25 @@ import com.skilldistillery.squadgoals.services.UserService;
 public class UserController {
 
 	@Autowired 
-	UserService userService;
+	private UserService userService;
+	
+	@Autowired
+	private AuthService authService;
 	
 	@GetMapping("users")
 	public List<User> index(HttpServletRequest req, HttpServletResponse res, Principal principal) {
 		System.out.println("****IN CONTROLLER - INDEX****");
 		List<User> users = userService.index("originaltom");
-		if (users.size() > 0) {
-			res.setStatus(200);
-			res.setHeader("Result", "Generated complete user list");
-		} else if (users.size() == 0) {
-			res.setStatus(404);
-			res.setHeader("Result", "No users found.");	
-		} else {
-			res.setStatus(400);
-			res.setHeader("Error", "Unable to generate user list");
-		}
+			if (users.size() > 0) {
+				res.setStatus(200);
+				res.setHeader("Result", "Generated complete user list");
+			} else if (users.size() == 0) {
+				res.setStatus(404);
+				res.setHeader("Result", "No users found.");	
+			} else {
+				res.setStatus(400);
+				res.setHeader("Error", "Unable to generate user list");
+			}
 		return users;
 	}
 	
@@ -50,7 +54,7 @@ public class UserController {
 		User user = userService.show(principal.getName(), id);
 		if (user == null) {
 			res.setStatus(404);
-			res.setHeader("Result", "No user found with id " + id);	
+			res.setHeader("Result", "User with id " + id + " does not exist");	
 		} else {
 			res.setStatus(200);
 			res.setHeader("Result", "Returned user id " + id);
@@ -60,34 +64,60 @@ public class UserController {
 	
 	@PutMapping("users/{id}")
 	public User update(HttpServletRequest req, HttpServletResponse res, @PathVariable int id, @RequestBody User user, Principal principal) {
-		User updated = null;
 		System.out.println("**** USER UPDATE ****" + principal);
-		try {
-			updated = userService.update(principal.getName(), id, user);
-			if (updated == null) {
-				res.setStatus(404);
-				res.setHeader("Error", "User with id " + id + " does not exist");
+		User updated = null;
+		if (authService.isLoggedInUser(principal.getName())) {
+			if (authService.isSameUser(principal.getName(), id) || authService.isAdmin(principal.getName())) {
+				if (authService.userExists(id)) {
+					try {
+						updated = userService.update(principal.getName(), id, user);
+						if (updated == null) {
+							throw new Exception();
+						} else {
+							res.setStatus(200);
+							res.setHeader("Result", "Updated user id " + id);
+						}
+					} catch (Exception e) {
+						res.setStatus(400);
+						res.setHeader("Error", "Unable to update user id " + id);
+						e.printStackTrace();
+					}
+				} else {
+					res.setStatus(404);
+					res.setHeader("Result", "User with id " + id + " does not exist");	
+				}
 			} else {
-				res.setStatus(200);
-				res.setHeader("Result", "Updated user id " + id);
+				res.setStatus(401);
+				res.setHeader("Error", "User does not have permission to perform this action");
 			}
-		} catch (Exception e) {
-			res.setStatus(400);
-			e.printStackTrace();
+		} else {
+			res.setStatus(401);
+			res.setHeader("Error", "Client must be logged in to perform this action");
 		}
 		return updated;
 	}
 	
 	@DeleteMapping("users/{id}")
 	public void destroy(HttpServletRequest req, HttpServletResponse res, @PathVariable int id, Principal principal) {
-		boolean deleted = userService.disable(principal.getName(), id);
-		if (deleted) {
-			res.setStatus(204);
+		if (authService.isLoggedInUser(principal.getName())) {
+			if (authService.isSameUser(principal.getName(), id) || authService.isAdmin(principal.getName())) {
+				if (authService.userExists(id)) {
+					boolean deleted = userService.disable(principal.getName(), id);
+					if (deleted) {
+						res.setStatus(204);
+						res.setHeader("Result", "Deactivated user id " + id);
+					}
+				} else {
+					res.setStatus(404);
+					res.setHeader("Result", "User with id " + id + " does not exist");	
+				}
+			} else {
+				res.setStatus(401);
+				res.setHeader("Error", "User does not have permission to perform this action");
+			}
 		} else {
-			res.setStatus(404);
+			res.setStatus(401);
+			res.setHeader("Error", "Client must be logged in to perform this action");
 		}
 	}
-	
-	
-	
 }
