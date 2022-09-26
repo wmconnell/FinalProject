e506e6d5796a625814e327c4979ad8ff912d6950
Ryan Donnelly <donnellyr2@gmail.com>
2022-09-26 09:25:48 -0600

    completed services

--- ngSquadGoals/src/app/services/badge.service.ts
+++ ngSquadGoals/src/app/services/badge.service.ts
@@ -1,9 +1,90 @@
+import { Badge } from './../models/badge';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BadgeService {
 
-  constructor() { }
+
+  private baseUrl = 'http://localhost:8088/'; // adjust port to match server
+  // private url = environment.baseUrl + 'api/badges'; // change 'todos' to your API path
+  private url = this.baseUrl + 'api/badges'; // change 'todos' to your API path
+  constructor(private http: HttpClient, private auth: AuthService) { }
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.auth.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+index(){
+  return this.http.get<Badge[]>(this.url,this.getHttpOptions()).pipe(catchError((err: any) => {
+    console.log(err);
+    return throwError(
+      () => new Error('BadgeService.index(): error retrieving Badges: ' + err)
+    );
+  })
+);
 }
+show(id:number){
+  return this.http.get<Badge>(this.url+'/'+id,this.getHttpOptions()).pipe(catchError((err: any) => {
+    console.log(err);
+    return throwError(
+      () => new Error('BadgeService.show(): error retrieving Badge: ' +id + err)
+    );
+  })
+  );
+
+}
+createBadge(badge: Badge){
+  return this.http.post<Badge>(this.url,badge, this.getHttpOptions()).pipe(catchError((err: any) => {
+    console.log(err);
+    return throwError(
+      () => new Error('BadgeService.createPark(): error creating Badge: '  + err)
+      );
+    })
+    );
+
+  }
+  updateBadge(badge: Badge, id:number){
+    return this.http.put<Badge>(this.url+'/'+id,badge, this.getHttpOptions()).pipe(catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error('BadgeService.updatePark(): error updating Badge: '  + err)
+        );
+      })
+      );
+
+    }
+    deleteBadge(id:number){
+      return this.http.delete<Badge>(this.url+'/'+id, this.getHttpOptions()).pipe(catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('BadgeService.deletePark(): error deleting Badge: ' +id + err)
+        );
+      })
+      );
+
+    }
+    showBadge(badgeId: any){
+      return this.http.get<Badge>("http://localhost:8088/api/badges/" + badgeId,this.getHttpOptions()).pipe(catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('BadgeService.showBadge(): error retrieving Badges: ' + err)
+        );
+      })
+    );
+    }
+  }
+
+
+
+
--- ngSquadGoals/src/app/services/tag.service.ts
+++ ngSquadGoals/src/app/services/tag.service.ts
@@ -1,9 +1,87 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { Tag } from '../models/tag';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TagService {
 
-  constructor() { }
+
+  private baseUrl = 'http://localhost:8088/'; // adjust port to match server
+  // private url = environment.baseUrl + 'api/tags'; // change 'todos' to your API path
+  private url = this.baseUrl + 'api/tags'; // change 'todos' to your API path
+  constructor(private http: HttpClient, private auth: AuthService) { }
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.auth.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+index(){
+  return this.http.get<Tag[]>(this.url,this.getHttpOptions()).pipe(catchError((err: any) => {
+    console.log(err);
+    return throwError(
+      () => new Error('TagService.index(): error retrieving Tags: ' + err)
+    );
+  })
+);
 }
+show(id:number){
+  return this.http.get<Tag>(this.url+'/'+id,this.getHttpOptions()).pipe(catchError((err: any) => {
+    console.log(err);
+    return throwError(
+      () => new Error('TagService.show(): error retrieving Tag: ' +id + err)
+    );
+  })
+  );
+
+}
+createTag(tag: Tag){
+  return this.http.post<Tag>(this.url,tag, this.getHttpOptions()).pipe(catchError((err: any) => {
+    console.log(err);
+    return throwError(
+      () => new Error('TagService.createPark(): error creating Tag: '  + err)
+      );
+    })
+    );
+
+  }
+  updateTag(tag: Tag, id:number){
+    return this.http.put<Tag>(this.url+'/'+id,tag, this.getHttpOptions()).pipe(catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error('TagService.updatePark(): error updating Tag: '  + err)
+        );
+      })
+      );
+
+    }
+    deleteTag(id:number){
+      return this.http.delete<Tag>(this.url+'/'+id, this.getHttpOptions()).pipe(catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('TagService.deletePark(): error deleting Tag: ' +id + err)
+        );
+      })
+      );
+
+    }
+    showTag(tagId: any){
+      return this.http.get<Tag>("http://localhost:8088/api/tags/" + tagId,this.getHttpOptions()).pipe(catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('TagService.showTag(): error retrieving Tags: ' + err)
+        );
+      })
+    );
+    }
+  }
+
