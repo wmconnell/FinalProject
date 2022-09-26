7df51fef8b60a527ff3ba867c5e974cdefb96828
GoldilocksZone <jmarulli@gmail.com>
2022-09-26 09:58:11 -0400

    Fixed localStorage issue

--- ngSquadGoals/src/app/components/user/user.component.ts
+++ ngSquadGoals/src/app/components/user/user.component.ts
@@ -12,80 +12,26 @@ import { Goal } from 'src/app/models/goal';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-users: User[] =[]
-loggedIn: User = new User();
-username: string | string = "admin";
-goals: Goal[] = []
-  constructor(private userService: UserService,private auth: AuthService,private router: Router, private route: ActivatedRoute, private goalService: GoalService) { }
+  loggedIn: User = new User();
+
+  constructor(private userService: UserService,private auth: AuthService,private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-
-
-    let idStr = this.route.snapshot.paramMap.get('username');
-    // let idStr = '4'
-    console.log(idStr);
-
-    if(idStr){
-
-      // got a todo ID, get and display todo
-      // let userId = Number.parseInt(idStr);
-      //   if(!isNaN(userId)){
-        this.userService.showUser(idStr).subscribe({
-
-          next: (user) =>{
-            console.log(user);
-
-            this.loggedIn =user;
-            console.log(this.loggedIn);
-
-            this.load();
-          },
-          error: (err) =>{
-            console.error('Error retriveing User:');
-            console.error(err);
-            this.router.navigateByUrl('noSuchUser')
-          }
-        })
-        }
-        else{
-          console.error('Invalid User Id: ' + idStr)
-        }
-    // }
-
+    this.load();
   }
 
-  setUser(username: string){
-    this.auth.getUser(username).subscribe({
-      next:(user) =>{
-        this.loggedIn =user;
+  load = (): void => {
+    this.auth.getLoggedInUser().subscribe(
+      {
+      next: (user) => {
+        this.loggedIn = user;
+        // console.log("Successfully retrieved user id " + user.id);
+      },
+      error: (err) => {
+        console.error("Unable to retrieve user: " + err);
       }
-    });
+    }
+    );
   }
-load(){
-  if(this.loggedIn){
-   this.goalService.index().subscribe({
-     next: (goal) =>{
-       this.goals = goal
-     },
-     error: (err) =>{
-       console.log(err);
-
-     }
-   })
-
-
-  }
-  // this.userService.show(this.loggedIn.id).subscribe({
-  //   next: (user) =>{
-  //     this.loggedIn =user
-  //   },
-  //   error: (problem) =>{
-  //     console.error("error loading users")
-  //     console.error(problem)
-  //   }
-
-
-  // })
-}
 
 }
